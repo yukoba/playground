3be8206aa78156ab73905cbc49e2d5f10271e081
Yu Kobayashi <yukoba@accelart.jp>
2016-04-14 16:14:30 +0900

    Increase numNeurons

--- playground.ts
+++ playground.ts
@@ -607,7 +607,7 @@ function addPlusMinusControl(x: number, layerIdx: number) {
       .attr("class", "mdl-button mdl-js-button mdl-button--icon")
       .on("click", () => {
         let numNeurons = state.networkShape[i];
-        if (numNeurons >= 8) {
+        if (numNeurons >= 50) {
           return;
         }
         state.networkShape[i]++;
